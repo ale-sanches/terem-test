2b82a274c39525ba3d814659c6d1ed12082ab502
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-03-18 22:11:26 +0300

    test-task: stage 2.2
    Button 2 functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-//по нажатию на "Кнопка 1" скрываем и показываем блолк с заголовком
+//по нажатию на "Кнопка 1" скрываем и показываем блок с заголовком
 $(".btn-warning").click(function () {
   $(".row-one").toggle();
 });
+
+//по нажатию на "Кнопка 2" меняем местами первые два блока второго ряда
+
+//реализация на JS
+// const secondBtn = document.querySelector(".btn-success");
+// secondBtn.addEventListener("click", function () {
+//   const rowTwoContainer = document.querySelector(".row-two");
+//   const rowTwoItems = rowTwoContainer.querySelectorAll(".row__item");
+//   const [firstItem, secondItem] = rowTwoItems;
+//   rowTwoContainer.insertBefore(secondItem, firstItem);
+//   console.log(rowTwoItems);
+// });
+
+//JQuery
+$(".btn-success").click(function () {
+  const firstItem = $(".row-two__item-one");
+  const secondItem = $(".row-two__item-two");
+
+  const firstItemCopy = firstItem.clone(true);
+  firstItem.replaceWith(secondItem.clone(true));
+  secondItem.replaceWith(firstItemCopy);
+});
+
+//при открытии страницы открывать модальное окно
+//JS
+
+//JQuery
+// $(function () {
+//   // действия, которые необходимо выполнить после загрузки документа...
+// });
