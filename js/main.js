85249aa940f4c223767cc22903f7c73170075fa9
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-03-19 11:15:29 +0300

    test-task: stage 2.3
    modal window added

--- js/main.js
+++ js/main.js
@@ -25,10 +25,42 @@ $(".btn-success").click(function () {
   secondItem.replaceWith(firstItemCopy);
 });
 
-//при открытии страницы открывать модальное окно
+////////////////////////////////////////////////////////////
+//при открытии страницы открываем модальное окно
 //JS
+// function toggleModalWindow() {
+//   document.querySelector(".modal-window").classList.toggle("hidden");
+//   document.querySelector(".overlay").classList.toggle("hidden");
+// }
+// document.addEventListener("DOMContentLoaded", toggleModalWindow);
+// //закрытие окна по клику на кнопку закрытия/фон/кнопку Esc
+// document
+//   .querySelector(".modal-window__close-btn")
+//   .addEventListener("click", toggleModalWindow);
+
+// document.querySelector(".overlay").addEventListener("click", toggleModalWindow);
+
+// document.addEventListener("keydown", function (event) {
+//   if (
+//     event.key == "Escape" &&
+//     !document.querySelector(".modal-window").classList.contains(".hidden")
+//   ) {
+//     toggleModalWindow();
+//   }
+// });
 
 //JQuery
-// $(function () {
-//   // действия, которые необходимо выполнить после загрузки документа...
-// });
+$(function () {
+  function toggleModalWindow() {
+    $(".modal-window").toggleClass("hidden");
+    $(".overlay").toggleClass("hidden");
+  }
+  $(document).on("click", ".modal-window__close-btn", toggleModalWindow);
+  $(".overlay").on("click", toggleModalWindow);
+  $(document).on("keydown", function (event) {
+    if (event.key === "Escape" && !$(".modal-window").hasClass("hidden")) {
+      toggleModalWindow();
+    }
+  });
+  toggleModalWindow();
+});
