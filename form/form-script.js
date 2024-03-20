c6a61cc543e5b022df754ff8f079dd74dbbe22d8
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-03-20 14:22:03 +0300

    test-task: stage 2.6

--- form/form-script.js
+++ form/form-script.js
@@ -1,58 +1,80 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const formElements = document.querySelectorAll('form input, form select, .form-select, textarea');
-    function validateForm() {
-        let isValid = true;
-        formElements.forEach(function (element) {
-            if (element.value === "") {
-                showErrorMessage(element);
-                isValid = false;
-            } else {
-                removeErrorMessage(element);
-            }
-        });
-        return isValid;
-    }
+  const formElements = document.querySelectorAll(
+    "form input, form select, .form-select, textarea"
+  );
 
-    function showErrorMessage(element) {
-        if (!element.parentNode.querySelector(".error-message")) {
-            const errorMessage = document.createElement("p");
-            errorMessage.innerHTML = "Вы ничего не ввели";
-            errorMessage.style.color = "red";
-            errorMessage.classList.add("error-message");
-            element.after(errorMessage);
-        }
-    }
-
-    function removeErrorMessage(element) {
-        if (element.parentNode.querySelector(".error-message")) {
-            element.parentNode.querySelector(".error-message").remove();
-        }
-    }
-
-    formElements.forEach(function (input) {
-        input.addEventListener('input', function () {
-            removeErrorMessage(input);
-        });
+  function validateForm() {
+    let isValid = true;
+    formElements.forEach(function (element) {
+      if (element.value === "") {
+        showErrorMessage(element);
+        isValid = false;
+      } else {
+        removeErrorMessage(element);
+      }
     });
-    //действия при нажатии кнопки
-    const submitBtn = document.querySelector(".submit-btn");
-    submitBtn.addEventListener("click", function (event) {
-        event.preventDefault();
+    return isValid;
+  }
 
-        const formData = {};
-        formElements.forEach(function (element) {
-            if (element.name) {
-                formData[element.name] = element.value;
-            }
-        });
+  function showErrorMessage(element) {
+    if (!element.parentNode.querySelector(".error-message")) {
+      const errorMessage = document.createElement("p");
+      errorMessage.innerHTML = "Вы ничего не ввели";
+      errorMessage.style.color = "red";
+      errorMessage.classList.add("error-message");
+      element.after(errorMessage);
+    }
+  }
 
-        if (validateForm()) {
-            const jsonHeading = document.querySelector(".heading");
-            jsonHeading.classList.remove("hidden");
-            const jsonData = JSON.stringify(formData);
-            const formInfo = document.querySelector('.json');
-            formInfo.textContent = jsonData;
-        }
+  function removeErrorMessage(element) {
+    if (element.parentNode.querySelector(".error-message")) {
+      element.parentNode.querySelector(".error-message").remove();
+    }
+  }
+
+  formElements.forEach(function (input) {
+    input.addEventListener("input", function () {
+      removeErrorMessage(input);
     });
+  });
+  //действия при нажатии кнопки
+  const submitBtn = document.querySelector(".submit-btn");
+  submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const formData = {};
+    formElements.forEach(function (element) {
+      if (element.name) {
+        formData[element.name] = element.value;
+      }
+    });
+
+    if (validateForm()) {
+      const jsonHeading = document.querySelector(".heading");
+      jsonHeading.classList.remove("hidden");
+      const jsonData = JSON.stringify(formData);
+      const formInfo = document.querySelector(".json");
+      formInfo.textContent = jsonData;
+
+      //запрос на сервер
+      const xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        "server.php?" + new URLSearchParams(formData).toString(),
+        true
+      );
+      xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          console.log(xhr.responseText);
+          alert("Success!");
+        } else {
+          console.error("Request failed with status:", xhr.status);
+        }
+      };
+      xhr.onerror = function () {
+        console.error("Request failed");
+      };
+      xhr.send(jsonData);
+    }
+  });
 });
-
