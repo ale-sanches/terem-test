6cfa66a929d3107a0165d1a537572ede58fe835a
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-03-28 17:49:21 +0300

    test-task: v.2.2

--- form/form-script.js
+++ form/form-script.js
@@ -1,41 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#form");
+  const jsonHeading = document.querySelector(".heading");
+  const formInfo = document.querySelector(".json");
 
-    const form = document.querySelector("#form");
-    const jsonHeading = document.querySelector(".heading");
-    const formInfo = document.querySelector(".json");
-
-//получаем введенные данные из формы и возвращаем их в json-формате
-    function makeJsonStructure() {
-        const jsonData = Object.fromEntries(new FormData(form));
-        return JSON.stringify(jsonData);
+  //получаем введенные данные из формы и возвращаем их в json-формате
+  function makeJsonStructure() {
+    const jsonData = Object.fromEntries(new FormData(form));
+    return JSON.stringify(jsonData);
+  }
+  //выводим json под формой
+  function showJson(json) {
+    jsonHeading.classList.remove("hidden");
+    formInfo.textContent = json;
+  }
+  //запрос к серверу
+  async function getResponse() {
+    const response = await fetch("server.php?");
+    if (response.ok) {
+      alert("Связь с сервером есть");
     }
-//выводим json под формой
-    function showJson(json) {
-        jsonHeading.classList.remove("hidden");
-        formInfo.textContent = json;
-    }
-//запрос к серверу
-    async function getResponse() {
-        try {
-            const response = await fetch("server.php?");
+  }
 
-            if (response.ok) {
-                alert("Связь с сервером есть");
-            } else {
-                alert("Ошибка при отправке: " + response.statusText);
-            }
-        } catch (error) {
-            alert("Ошибка при выполнении запроса: " + error.message);
-        }
-    }
-
-    //действия при отправке формы
-    form.addEventListener("submit", async function (evt) {
-        evt.preventDefault();
-        const json = makeJsonStructure();
-        showJson(json);
-        await getResponse();
-    });
-
-})
+  //действия при отправке формы
+  form.addEventListener("submit", async function (evt) {
+    evt.preventDefault();
+    const json = makeJsonStructure();
+    showJson(json);
+    await getResponse();
+  });
+});
 
