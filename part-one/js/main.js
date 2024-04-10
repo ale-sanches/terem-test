95058cbdda7fcd677245b87f6e7eb96061ad8f8a
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-10 13:03:50 +0300

    test-task: part-one html,css and js files updated

--- part-one/js/main.js
+++ part-one/js/main.js
@@ -1,46 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const firstBtn = document.querySelector(".btn-warning");
-  const secondBtn = document.querySelector(".btn-success");
-  const modalWindow = document.querySelector(".modal-window");
-  const modalCloseBtn = document.querySelector(".modal-window__close-btn");
-  const rowOne = document.querySelector(".row-one");
+const firstBtn = document.querySelector(".btn-warning");
+const secondBtn = document.querySelector(".btn-success");
+const modalWindowOverlay = document.querySelector(".modal-window__overlay");
+const modalCloseBtn = document.querySelector(".modal-window__close-btn");
+const rowOne = document.querySelector(".row-one");
 
-  function toggleItem(element) {
-    element.classList.toggle("hidden");
-  }
+function toggleItem(element) {
+  element.classList.toggle("hidden");
+}
 
-  //по нажатию на "Кнопка 1" скрываем и показываем блок с заголовком
-  firstBtn.addEventListener("click", function () {
-    toggleItem(rowOne);
-  });
-
-  //по нажатию на "Кнопка 2" меняем местами первые два блока второго ряда
-  secondBtn.addEventListener("click", function () {
-    const [firstItem, secondItem] = document.querySelectorAll(
-      ".row-two .row__item"
-    );
-    secondItem.after(firstItem);
-  });
-
-  //закрытие модального окна
-
-  function closeModalWindow() {
-    toggleItem(modalWindow);
-  }
-
-  function handleBackgrdClick(event) {
-    if (event.target === modalWindow) {
-      closeModalWindow();
-    }
-  }
-
-  function handleEscClick(event) {
-    if (event.key === "Escape" && !modalWindow.classList.contains("hidden")) {
-      closeModalWindow();
-    }
-  }
-
-  modalCloseBtn.addEventListener("click", closeModalWindow);
-  modalWindow.addEventListener("click", handleBackgrdClick);
-  document.addEventListener("keydown", handleEscClick);
+//по нажатию на "Кнопка 1" скрываем и показываем блок с заголовком
+firstBtn.addEventListener("click", function () {
+  toggleItem(rowOne);
 });
+
+//по нажатию на "Кнопка 2" меняем местами первые два блока второго ряда
+secondBtn.addEventListener("click", function () {
+  const [firstItem, secondItem] = document.querySelectorAll(
+    ".row-two .row__item"
+  );
+  secondItem.after(firstItem);
+});
+
+//закрытие модального окна
+
+function handleModalClick(event) {
+  if (event.target === modalWindowOverlay) {
+    toggleItem(modalWindowOverlay);
+  }
+}
+
+function handleEscKey(event) {
+  if (
+    event.key === "Escape" &&
+    !modalWindowOverlay.classList.contains("hidden")
+  ) {
+    toggleItem(modalWindowOverlay);
+  }
+}
+
+modalCloseBtn.addEventListener("click", () => {
+  toggleItem(modalWindowOverlay);
+});
+modalWindowOverlay.addEventListener("click", handleModalClick);
+document.addEventListener("keydown", handleEscKey);
