75eae4668e1da652606e7af992f253938f23788a
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-11 21:09:29 +0300

    test-task: form script and styles v.3

--- form/form-script.js
+++ form/form-script.js
@@ -1,40 +1,49 @@
 const form = document.querySelector("#form");
-const jsonHeading = document.querySelector(".heading");
-const formInfo = document.querySelector(".json");
-const SERVER_URL = "server.php";
-
-//получаем введенные данные из формы и возвращаем их в json-формате
-// function serializeToJSON() {
-//     const jsonData = Object.fromEntries(new FormData(form));
-//     return JSON.stringify(jsonData);
-// }
-const jsonData = () => JSON.stringify(Object.fromEntries(new FormData(form)));
-//выводим json под формой
-function showJson(json) {
-  jsonHeading.classList.remove("hidden");
-  formInfo.textContent = json;
-}
-
-//запрос к серверу
-async function sendForm(jsonData) {
-  try {
-    const urlParams = new URLSearchParams(jsonData);
-    const response = await fetch(`${SERVER_URL}?${urlParams}`);
-    if (!response.ok) {
-      throw new Error("Ошибка при отправке данных на сервер");
-    } else {
-      alert("Ok"); // Выводим ответ через alert
-      console.log(jsonData);
-    }
-  } catch (error) {
-    console.error(error);
-    alert("Произошла ошибка: " + error.message);
-  }
-}
+const serverUrl = "server.php";
 
 form.addEventListener("submit", async function (evt) {
-  evt.preventDefault();
-  const jsonDataValue = jsonData();
-  showJson(jsonData());
-  await sendForm(jsonDataValue);
+    evt.preventDefault();
+
+    const json = serializeFormToJSON(form);
+    displayFormInfo(json);
+
+    try {
+        let resp = await performGetRequest(serverUrl, json);
+        if (!resp.ok) {
+            throw new Error("Якая оказия приключилась, статус " + resp.status)
+        }
+        const responseBody = await resp.text();
+        alert("Тут могло быть ваше тело ответа: {" + responseBody + "}");
+    } catch (error) {
+        console.log(error.stack)
+    }
 });
+
+/**
+ * Сериализует данные переданной формы в JSON
+ * @param form форма, данные который нужно собрать в JSON
+ * @returns {string} данные формы, сериализованные в JSON
+ */
+function serializeFormToJSON(form) {
+    return JSON.stringify(Object.fromEntries(new FormData(form)));
+}
+
+/**
+ * Отображает переданный JSON в блок вывода сериализованной формы
+ * @param json сериализованные данные для отображения
+ */
+function displayFormInfo(json) {
+    document.querySelector(".heading").classList.remove("hidden");
+    document.querySelector(".json").textContent = json;
+}
+
+/**
+ * Выполняет GET запрос на получение информации по собранным данным с формы
+ * @param serverURL url сервера для отправки запроса
+ * @param jsonParam параметры запроса
+ * @returns {Promise<Response>} потенциальный ответ от сервера
+ */
+async function performGetRequest(serverURL, jsonParam) {
+    let urlParams = new URLSearchParams(jsonParam);
+    return await fetch(`${serverURL}?${urlParams}`);
+}
