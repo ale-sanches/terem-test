274aeacd609c2ac60c038056f9a27a0a9d1f0809
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-11 16:45:44 +0300

    test-task: form-script intermediate variant

--- form/form-script.js
+++ form/form-script.js
@@ -1,32 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("#form");
-  const jsonHeading = document.querySelector(".heading");
-  const formInfo = document.querySelector(".json");
+const form = document.querySelector("#form");
+const jsonHeading = document.querySelector(".heading");
+const formInfo = document.querySelector(".json");
+const SERVER_URL = "server.php";
 
-  //получаем введенные данные из формы и возвращаем их в json-формате
-  function makeJsonStructure() {
-    const jsonData = Object.fromEntries(new FormData(form));
-    return JSON.stringify(jsonData);
-  }
-  //выводим json под формой
-  function showJson(json) {
-    jsonHeading.classList.remove("hidden");
-    formInfo.textContent = json;
-  }
-  //запрос к серверу
-  async function getResponse() {
-    const response = await fetch("server.php?");
-    if (response.ok) {
-      alert("Связь с сервером есть");
+//получаем введенные данные из формы и возвращаем их в json-формате
+// function serializeToJSON() {
+//     const jsonData = Object.fromEntries(new FormData(form));
+//     return JSON.stringify(jsonData);
+// }
+const jsonData = () => JSON.stringify(Object.fromEntries(new FormData(form)));
+//выводим json под формой
+function showJson(json) {
+  jsonHeading.classList.remove("hidden");
+  formInfo.textContent = json;
+}
+
+//запрос к серверу
+async function sendForm(jsonData) {
+  try {
+    const urlParams = new URLSearchParams(jsonData);
+    const response = await fetch(`${SERVER_URL}?${urlParams}`);
+    if (!response.ok) {
+      throw new Error("Ошибка при отправке данных на сервер");
+    } else {
+      alert("Ok"); // Выводим ответ через alert
+      console.log(jsonData);
     }
+  } catch (error) {
+    console.error(error);
+    alert("Произошла ошибка: " + error.message);
   }
+}
 
-  //действия при отправке формы
-  form.addEventListener("submit", async function (evt) {
-    evt.preventDefault();
-    const json = makeJsonStructure();
-    showJson(json);
-    await getResponse();
-  });
+form.addEventListener("submit", async function (evt) {
+  evt.preventDefault();
+  const jsonDataValue = jsonData();
+  showJson(jsonData());
+  await sendForm(jsonDataValue);
 });
-
